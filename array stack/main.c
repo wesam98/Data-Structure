bf476452c7ed0ab4feee5665e3bb25c8cbf1b422
wesam98 <120117841+wesam98@users.noreply.github.com>
2023-07-22 17:27:48 +0300

    Update main.c

--- array stack/main.c
+++ array stack/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "stack.h"
-void display (stackEntry e)  {
-    printf("element is %d\n",e);     // only user know type of stackEntry so we make user build this function
-}
+void display (stackEntry e) ;
 int main()
 { stackEntry e;  // will be determined by the user
     Stack s;
     createStack(&s);//initialize stack
     printf("enter element to be pushed");
     scanf("%d",&e);
-    if(!stackFull(s)){
+
+    if(!stackFull(&s)){
         push(e,&s);
     }
    /* if(!stackEmpty(&s))
     {
         pop(&e ,&s);
-    }*/
+    }
     if(!stackEmpty(&s))
     {
         stackTop(&e ,&s);
-    }
+    }*/
     traversstack(&display,&s);
+    printf("stack size is %d \n", stackSize(&s));
 
     return 0;
+}
+void display (stackEntry e)  {
+    printf("element is %d\n",e);     // only user know type of stackEntry so we make user build this function
 }
